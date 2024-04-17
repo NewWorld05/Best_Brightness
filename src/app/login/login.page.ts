da86fa70d5c705294131462848af4199a6c118aa
Kwenzakwenkosi <kwenza05@gmail.com>
2024-04-17 11:48:37 +0200

    commit

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { Router } from '@angular/router';
 import { ToastController } from '@ionic/angular';
+import { GoogleAuthProvider } from 'firebase/auth';
 
 @Component({
   selector: 'app-login',
@@ -18,16 +19,31 @@ export class LoginPage implements OnInit {
   ngOnInit() {
   }
 
+
+
   async log() {
     try {
       await this.auth.signInWithEmailAndPassword(this.email, this.password);
+      window.alert('Logged in successfully');
       this.presentToast('Logged in successfully');
       this.router.navigateByUrl('/main');
     } catch (error) {
       console.error(error); // Log the entire error object to inspect its structure
-      this.presentToast('An error occurred during login'); // Display a generic error message
+      window.alert(error);
+      this.presentToast('An error occurred during login - Please check your login details and try again'); // Display a generic error message
     }
   }
+
+  // async loginWithGoogle() {
+  //   try {
+  //     const user = await this.authService.loginWithGoogle();
+  //     // Handle successful login (e.g., navigate to another page)
+  //     this.navCtrl.navigateForward('/dispatch');
+  //     console.log('Logged in user:', user);
+  //   } catch (err) {
+  //     console.error('Login error:', err);
+  //   }
+  // }
 
   async presentToast(message: string) {
     const toast = await this.toastController.create({
